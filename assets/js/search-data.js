c4fc7c93d3a609060fc5812626d2f1c1d2afdc35
YangdiJiang <65643288+YangdiJiang@users.noreply.github.com>
2025-04-27 04:42:05 +0000

    Deploying to gh-pages from @ YangdiJiang/YangdiJiang.github.io@9f60adae8a16315d845a7ef999f8c9e320a7de1c 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -473,35 +473,14 @@ ninja.data = [{
         title: 'email',
         section: 'Socials',
         handler: () => {
-          window.open("mailto:%79%6F%75@%65%78%61%6D%70%6C%65.%63%6F%6D", "_blank");
-        },
-      },{
-        id: 'social-inspire',
-        title: 'Inspire HEP',
-        section: 'Socials',
-        handler: () => {
-          window.open("https://inspirehep.net/authors/1010907", "_blank");
-        },
-      },{
-        id: 'social-rss',
-        title: 'RSS Feed',
-        section: 'Socials',
-        handler: () => {
-          window.open("/feed.xml", "_blank");
+          window.open("mailto:%79%61%6E%67%64%69@%75%61%6C%62%65%72%74%61.%63%61", "_blank");
         },
       },{
         id: 'social-scholar',
         title: 'Google Scholar',
         section: 'Socials',
         handler: () => {
-          window.open("https://scholar.google.com/citations?user=qc6CJjYAAAAJ", "_blank");
-        },
-      },{
-        id: 'social-custom_social',
-        title: 'Custom_social',
-        section: 'Socials',
-        handler: () => {
-          window.open("https://www.alberteinstein.com/", "_blank");
+          window.open("https://scholar.google.com/citations?user=OfZ985EAAAAJ", "_blank");
         },
       },{
       id: 'light-theme',
